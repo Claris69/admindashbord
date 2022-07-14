ea4ff7f1358bd29e96e8291afb57515786313695
Claris69 <rathmc6@gmail.com>
2022-07-14 12:20:43 +0300

    added php syntax

--- webStartingTemplate/edit-enrollment.php
+++ webStartingTemplate/edit-enrollment.php
@@ -40,9 +40,11 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-header bg-dark text-center text-white">
-                            <h4 >Edit student <?php echo $fullname ?></h4>
+                        <div class="card-header bg-dark text-center text-white " >
+						
+                            <h4> Edit student <?php echo $fullname ?></h4>
 							<?php echo $message ?>
+							<a href="students.php" class="float-start btn btn-outline-primary btn-sm "><i class="fa fa-arrow-left"></i> Return</a>
                         </div>
 						<!-- <div class="card-body"> -->
 		                    <form action="edit-enrollment.php?id=<?php echo $id?>" method="POST" >
@@ -65,7 +67,7 @@
 									<div class="col-lg-6">
 										<label for="gender"  class="form-label"><h6>what is your gender ?</h6></label> 
 														
-										<select class="custom-select" name="gender">
+										<select class="form-select" name="gender">
 											<option selected><?php echo $gender ?></option>
 											<option value="male">male</option>
 											<option value="female">female</option>
@@ -78,7 +80,7 @@
 										
 										<label for="course" name="course" class="form-label"><h6>What is your prefered course ?</h6></label> 
 													
-										<select class="custom-select" name="course">
+										<select class="form-select" name="course">
 											<option selected><?php echo $course ?></option>
 											<option value="software design">software design</option>
 											<option value="cyber security">cyber security</option>
